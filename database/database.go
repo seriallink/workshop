8a51e7c2246ed7ecfe1da1b050ed53fc1fbadef5
Marcelo Monaco <marcelo.monaco@gmail.com>
2016-02-29 00:14:44 -0300

    Fixed!

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ func Init() {
     var err error
 
     if db, err = sql.Open("sqlite3", "workshop.sqlite"); err == nil {
-        if _, err = db.Exec("create table if not exists books(id text, title text, author text)"); err == nil {
+        if _, err = db.Exec("create table if not exists users(id text, name text, email text, password text)"); err == nil {
             _, err = CreateUser()
         }
     }
